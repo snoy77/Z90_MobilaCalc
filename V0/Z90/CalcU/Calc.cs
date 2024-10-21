75c3511caadc23521c8031da2cbc0fc1e2d33553
snoy77 <snoy77@mail.ru>
2024-10-21 12:39:25 +0500

    m

---
+++ V0/Z90/CalcU/Calc.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcU
+{
+    public class Calc
+    {
+        //0 - Ввод первого числа
+        //  0 -> 1, Если ввод операции. Запоминаем операцию. Записываем в первое число
+        //1 - Ввод второго числа
+        //  1 -> 2, Если ввод "=". Записываем второе число, выполняем вычисление
+        //  1 -> 1, Если ввод операции: записываем второе число, выполняем вычисление, записываем команду, обнуляем актуальный ввод
+        //2 - Работа с запомненными числами
+        //  2 -> 2, Если встречаем "=":         выполняем вычисление.
+        //  2 -> 1, Если встречаем операцию:    записываем операцию, обнуляем актуальный ввод
+        //  2 -> 1, Если встерчаем число:       Обнуляем актуальный ввод
+        //  2 -> 0, Если встречаем инверсию:    Ставим инверсию и начинаем новый ввод
+        //  2 -> 0, Если встречаем разделитель: Обнуляем ввод, ставим разделитель, начинаем нвоый ввод
+
+        private int status;
+
+        private string command;             //Выполняемая операция
+
+        private string input_actual;        //Актуальный ввод
+        private string input_actual_null;   //Ввод по-умолчанию
+        private float digit_a;              //Первое число
+        private float digit_b;              //Второе число
+
+        public const string input_separator = ",";
+        public const string input_negative = "-+";
+
+        public string output = "";
+
+        public int Status { get { return this.status; } }
+        public string Command { get { return this.command; } }
+        public float Digit_a { get { return this.digit_a; } }
+        public float Digit_b { get { return this.digit_b; } }
+        public string Input_actual { get { return this.input_actual; } }
+        private float Digit_actual { get { return float.Parse(this.input_actual); } }
+
+        public Calc()
+        {
+            this.status = 0;
+            this.input_actual_null = "";
+
+            digit_actual_reset();
+        }
+
+        public void digit_actual_reset() { this.input_actual = this.input_actual_null; }
+
+        public void DoCommand()
+        {
+            if(( this.digit_b == 0) && this.command == "/") 
+            {
+                digit_actual_reset();
+                this.digit_a = 0;
+                this.digit_b = 0;
+                this.status = 0;
+                return;
+            }
+
+            switch (this.command)
+            {
+                case "+":
+                    {
+                        this.digit_a = this.digit_a + this.digit_b;
+                        break;
+                    }
+                case "-":
+                    {
+                        this.digit_a = this.digit_a - this.digit_b;
+                        break;
+                    }
+                case "/":
+                    {
+                        this.digit_a = this.digit_a / this.digit_b;
+                        break;
+                    }
+                case "x":
+                    {
+                        this.digit_a = this.digit_a * this.digit_b;
+                        break;
+                    }
+            }
+            this.input_actual = this.digit_a.ToString();
+        }
+        private bool CanAddSeparator()
+        {
+            for (int i = 0; i < this.input_actual.Length; i++)
+            {
+                if (this.input_actual[i].ToString() == Calc.input_separator)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void InputSeparator(string symbol)
+        {
+            if (this.CanAddSeparator())
+            {
+                this.input_actual += symbol;
+            }
+        }
+        private void InputDigit(string symbol)
+        {
+            if (this.input_actual == this.input_actual_null)
+            {
+                this.input_actual = symbol;
+            }
+            else
+            {
+                this.input_actual += symbol;
+            }
+        }
+
+        public void DoWithSymbol(string symbol)
+        {
+            switch (this.Status)
+            {
+                case 0:
+                    {
+                        switch (symbol)
+                        {
+                            case "-":
+                            case "/":
+                            case "x":
+                            case "+":
+                                {
+                                    this.digit_a = this.Digit_actual;
+                                    this.digit_actual_reset();
+                                    this.command = symbol;
+
+                                    this.status = 1;
+                                    break;
+                                }
+                            case input_separator:
+                                {
+                                    this.InputSeparator(symbol);
+                                    break;
+                                }
+                            case input_negative:
+                                {
+                                    if (this.input_actual != this.input_actual_null)
+                                    {
+                                        if (this.input_actual[0] != '-')
+                                        {
+                                            this.input_actual = "-" + this.input_actual;
+                                        }
+                                        else
+                                        {
+                                            this.input_actual = this.input_actual.Substring(1);
+                                        }
+                                    }
+                                    break;
+                                }
+                            case "=":
+                                { break; }
+                            default:
+                                {
+                                    this.InputDigit(symbol);
+                                    break;
+                                }
+                        }
+                    }
+                    break;
+
+                case 1:
+                    {
+                        switch (symbol)
+                        {
+                            case "=":
+                                {
+                                    if(this.Input_actual == this.input_actual_null)
+                                    {
+                                        this.digit_b = this.digit_a;
+                                    }
+                                    else
+                                    {
+                                        this.digit_b = this.Digit_actual;
+                                    }
+
+                                    this.status = 2;
+                                    this.DoCommand();
+
+                                 
+
+                                    break;
+                                }
+                            case "+":
+                            case "-":
+                            case "x":
+                            case "/":
+                                {
+                                    this.digit_b = this.Digit_actual;
+
+                                    this.DoCommand();
+
+                                    this.command = symbol;
+                                    this.digit_actual_reset();
+
+                                    break;
+                                }
+                            case input_separator:
+                                {
+                                    this.InputSeparator(symbol);
+                                    break;
+                                }
+                            case input_negative:
+                                {
+                                    if (this.input_actual != this.input_actual_null)
+                                    {
+                                        if (this.input_actual[0] != '-')
+                                        {
+                                            this.input_actual = "-" + this.input_actual;
+                                        }
+                                        else
+                                        {
+                                            this.input_actual = this.input_actual.Substring(1);
+                                        }
+                                    }
+                                    break;
+                                }
+                            default:
+                                {
+                                    this.InputDigit(symbol);
+                                    break;
+                                }
+                        }
+
+                        break;
+                    }
+                case 2:
+                    {
+                        switch (symbol)
+                        {
+                            case "=":
+                                {
+                                    this.DoCommand();
+                                    break;
+                                }
+                            case "+":
+                            case "-":
+                            case "x":
+                            case "/":
+                                {
+                                    this.command = symbol;
+                                    this.digit_actual_reset();
+
+                                    this.status = 1;
+                                    break;
+                                }
+                            case input_separator:
+                                {
+                                    this.input_actual = "0" + Calc.input_separator;
+                                    this.status = 0;
+                                    break;
+                                }
+                            case input_negative:
+                                {
+                                    if (this.input_actual != this.input_actual_null)
+                                    {
+                                        if (this.input_actual[0] != '-')
+                                        {
+                                            this.input_actual = "-" + this.input_actual;
+                                        }
+                                        else
+                                        {
+                                            this.input_actual = this.input_actual.Substring(1);
+                                        }
+                                    }
+
+                                    this.status = 0;
+                                    break;
+                                }
+                            default:
+                                {
+                                    this.input_actual = symbol;
+                                    this.status = 0;
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+
+                default: break;
+            }
+        }
+    }
+}
---
+++ V0/Z90/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CalcU;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void Test_InputDigitOne()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("3");
+            Assert.AreEqual("3", calc.Input_actual);
+            Assert.AreEqual("3", calc.Input_actual);
+        }
+        [TestMethod]
+        public void Test_InputDigitSecond()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("2");
+            Assert.AreEqual("32", calc.Input_actual);
+        }
+        [TestMethod]
+        public void Test_digitFirstAndOperation_plus()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            Assert.AreEqual("", calc.Input_actual);
+            Assert.AreEqual("+", calc.Command);
+            Assert.AreEqual((float)12, calc.Digit_a);
+        }
+        [TestMethod]
+        public void Test_plusOnTwoDigit()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            Assert.AreEqual((float)12, calc.Digit_a);
+        }
+        [TestMethod]
+        public void Test_plusOnTwoDigit_andCalculated()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("=");
+            Assert.AreEqual((float)12 + (float)34, calc.Digit_a);
+        }
+        [TestMethod]
+        public void Test_plusOnTwoDigit_andCalculatedSecond()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("=");
+            calc.DoWithSymbol("=");
+            Assert.AreEqual((float)12 + (float)34 + (float)34, calc.Digit_a);
+        }
+        [TestMethod]
+        public void Test_plusOnTwoDigit_andCalculatedThird()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("=");
+            calc.DoWithSymbol("=");
+            calc.DoWithSymbol("=");
+            Assert.AreEqual((float)12 + (float)34 + (float)34 + (float)34, calc.Digit_a);
+        }
+        [TestMethod]
+        public void Test_plusOnTwoDigit_andPlusAgain()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("+");
+            Assert.AreEqual((float)12 + (float)34, calc.Digit_a);
+        }
+        [TestMethod]
+        public void Test_plusOnTwoDigit_andPlusNewDigit()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("5");
+            calc.DoWithSymbol("6");
+            Assert.AreEqual(1, calc.Status);
+            Assert.AreEqual((float)12 + (float)34, calc.Digit_a);
+            Assert.AreEqual("56", calc.Input_actual);
+        }
+        [TestMethod]
+        public void Test_CalculatedTwoDigit_AndNewInput()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("=");
+            calc.DoWithSymbol("7");
+            calc.DoWithSymbol("8");
+            Assert.AreEqual(0, calc.Status);
+            Assert.AreEqual("78", calc.Input_actual);
+        }
+        [TestMethod]
+        public void Test_CalculatedTwoDigit_AndNewInputWithMinus()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("=");
+            calc.DoWithSymbol("7");
+            calc.DoWithSymbol("8");
+            calc.DoWithSymbol("-");
+            Assert.AreEqual(1, calc.Status);
+            Assert.AreEqual((float)78, calc.Digit_a);
+            Assert.AreEqual("", calc.Input_actual);
+        }
+        [TestMethod]
+        public void Test_CalculatedTwoDigit_AndNewInputWithMinusDigit()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("=");
+            calc.DoWithSymbol("7");
+            calc.DoWithSymbol("8");
+            calc.DoWithSymbol("-");
+            calc.DoWithSymbol("9");
+            Assert.AreEqual(1, calc.Status);
+            Assert.AreEqual((float)78, calc.Digit_a);
+            Assert.AreEqual("9", calc.Input_actual);
+        }
+        [TestMethod]
+        public void Test_CalculatedTwoReshenia()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("1");
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("3");
+            calc.DoWithSymbol("4");
+            calc.DoWithSymbol("=");
+            calc.DoWithSymbol("7");
+            calc.DoWithSymbol("8");
+            calc.DoWithSymbol("-");
+            calc.DoWithSymbol("9");
+            calc.DoWithSymbol("=");
+            Assert.AreEqual(2, calc.Status);
+            Assert.AreEqual((float)69, calc.Digit_a);
+            Assert.AreEqual("69", calc.Input_actual);
+        }
+        [TestMethod]
+        public void Test_DigitWithHimself()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("+");
+            calc.DoWithSymbol("=");
+            Assert.AreEqual(2, calc.Status);
+            Assert.AreEqual((float)4, calc.Digit_a);
+            Assert.AreEqual("4", calc.Input_actual);
+        }
+        [TestMethod]
+        public void Test_DelitNaNol()
+        {
+            Calc calc = new Calc();
+
+            calc.DoWithSymbol("2");
+            calc.DoWithSymbol("/");
+            calc.DoWithSymbol("0");
+            calc.DoWithSymbol("=");
+            Assert.AreEqual(0, calc.Status);
+            Assert.AreEqual((float)0, calc.Digit_a);
+            Assert.AreEqual("", calc.Input_actual);
+        }
+    }
+}
